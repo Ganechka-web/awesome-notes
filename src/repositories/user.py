34a94a5312c29dd4d8ed5c2aa3e55efbcc25e937
Ganechka <maksim15848@gmail.com>
2025-05-02 16:23:40 +0300

    feat(repositories): user repository

---
+++ src/repositories/user.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy import select
+
+from models.user import User
+
+
+class UserRepository:
+    model = User
+
+    def __init__(self, engine: AsyncEngine):
+        self.engine = engine
+
+    async def get_all(self) -> list[User]:
+        async with AsyncSession(self.engine) as session:
+            query = select(User)
+            users = await session.scalars(query)
+
+            return users.all()
+
+    async def get_one_by_id(self, id: int) -> User:
+        async with AsyncSession(self.engine) as session:
+            query = select(User).where(id=id)
+            user = await session.execute(query)
+
+            return user.scalar_one_or_none()
+
+    async def get_one_by_username(self, username: str) -> User:
+        async with AsyncSession(self.engine) as session:
+            query = select(self.model).where(username == username)
+            user = await session.execute(query)
+
+            return user.scalar_one_or_none()      
+
+    async def create_one(self, user: User) -> int:
+        async with AsyncSession(self.engine) as session:
+            session.add(user)
+            await session.commit()
+            
+            return user.id
