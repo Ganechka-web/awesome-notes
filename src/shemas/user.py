9eb5b37b6e108cbbdd276b4755797fbefac8e063
Ganechka <maksim15848@gmail.com>
2025-05-03 12:19:15 +0300

    fix(shemas): add joined_at and updated_at fields, formatting

--- src/shemas/user.py
+++ src/shemas/user.py
@@ -6,16 +6,21 @@ from pydantic import BaseModel, Field
 class UserCreateShema(BaseModel):
     username: str = Field(max_length=50)
     gender: str
-    age: int 
+    age: int
 
     class Config:
         from_attributes = True
-    
+
+
 class UserUpgrateShema(UserCreateShema):
     pass
 
+
 class UserOutputShema(UserCreateShema):
     id: int
+    joined_at: datetime
+    updated_at: datetime
+
 
 class UserShema(UserOutputShema):
     password: str
