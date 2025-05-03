08722ae9a555326e81650a6217224544f4ba3c8d
Ganechka <maksim15848@gmail.com>
2025-05-03 14:07:12 +0300

    feat(api.endpoints): add update and delete handlers

--- src/api/endpoints/user.py
+++ src/api/endpoints/user.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Path
 
 from core.database import async_engine
 from repositories.user import UserRepository
 from services.user import UserService
-from shemas.user import UserOutputShema, UserCreateShema
+from shemas.user import (
+    UserOutputShema, 
+    UserCreateShema,
+    UserUpgrateShema
+)
 
 
 users_router = APIRouter(prefix='/users')
@@ -24,3 +30,16 @@ async def create_one(new_user: UserCreateShema):
     new_user_id = await user_service.create_one(new_user=new_user)
 
     return new_user_id
+
+
+@users_router.patch('/update/{user_id}')
+async def update_one(user_id: Annotated[int, Path()], 
+                     updated_user: UserUpgrateShema):
+    await user_service.update_one(user_id=user_id,
+                                  updated_user=updated_user)
+
+
+@users_router.delete('/delete/{user_id}')
+async def delete_one(user_id: Annotated[int, Path()]):
+    await user_service.delete_one(user_id=user_id)
+    
