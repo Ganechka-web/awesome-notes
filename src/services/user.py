f2cbbebe12ac39f39cd24a883fde7b06c6824ae6
Ganechka <maksim15848@gmail.com>
2025-05-05 10:12:14 +0300

    feat(services): exceptions handing

--- src/services/user.py
+++ src/services/user.py
@@ -5,6 +5,11 @@ from shemas.user import (
 ) 
 from models.user import User
 from repositories.user import UserRepository
+from exceptions.repositories import DataBaseError
+from exceptions.services import ( 
+    UserNotFoundError, 
+    UserAlreadyExistsError
+)
 
 
 class UserService:
@@ -18,30 +23,61 @@ class UserService:
                 for user in users]
     
     async def get_one_by_id(self, id: int) -> UserOutputShema | None:
-        user = await self.repository.get_one_by_id(id=id)
+        try:
+            user = await self.repository.get_one_by_id(id=id)
+        except DataBaseError as e:
+            raise UserNotFoundError(
+                f'Unable to find user with id - {id}'
+            ) from e
 
         return UserOutputShema.model_validate(user)
     
     async def get_one_by_username(self, 
                                   username: str) -> UserOutputShema | None:
-        user = await self.repository.get_one_by_username(username=username)
+        try:
+            user = await self.repository.get_one_by_username(username=username)
+        except DataBaseError as e:
+            raise UserNotFoundError(
+                f'Unable to find user with username - {username}'
+            ) from e
 
         return UserOutputShema.model_validate(user)
 
     async def create_one(self, new_user: UserCreateShema) -> int:
         new_user_orm = User(**new_user.__dict__)
-        new_user_id = await self.repository.create_one(user=new_user_orm)
+        try:
+            new_user_id = await self.repository.create_one(user=new_user_orm)
+        except DataBaseError as e:
+            raise UserAlreadyExistsError(
+                f'User with username - {new_user.username} already exists'
+            ) from e
 
         return new_user_id
     
     async def update_one(self, user_id: int, 
                          updated_user: UserUpgrateShema) -> None:
-        current_user = await self.repository.get_one_by_id(id=user_id)
+        try: 
+            current_user = await self.repository.get_one_by_id(id=user_id)
+        except DataBaseError as e:
+            raise UserNotFoundError(
+                f'Unable to find user with id - {user_id}'
+            ) from e
+        
         for field, value in updated_user.model_dump(exclude_unset=True).items():
             setattr(current_user, field, value)
 
-        await self.repository.update_one(user=current_user)
+        try:
+            await self.repository.update_one(user=current_user)
+        except DataBaseError as e:
+            raise UserAlreadyExistsError(
+                f'User with username - {updated_user.username} already exists'
+            ) from e
 
     async def delete_one(self, user_id) -> None:
-        user_on_delete = await self.repository.get_one_by_id(id=user_id)
+        try: 
+            user_on_delete = await self.repository.get_one_by_id(id=user_id)
+        except DataBaseError as e:
+            raise UserNotFoundError(
+                f'Unable to find user with id - {user_id}'
+            ) from e
         await self.repository.delete_one(user=user_on_delete) 
