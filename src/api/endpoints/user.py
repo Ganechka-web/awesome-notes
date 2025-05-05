7c5a8716bd7dba70d16475c4ec5d3b8a46c999c9
Ganechka <maksim15848@gmail.com>
2025-05-05 10:13:06 +0300

    feat(api.endpoints): exceptions handing and processing

--- src/api/endpoints/user.py
+++ src/api/endpoints/user.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Path
+from fastapi import (
+    APIRouter, Path, 
+    HTTPException, status
+)
 
 from core.database import async_engine
 from repositories.user import UserRepository
 from services.user import UserService
+from exceptions.services import (
+    UserNotFoundError,
+    UserAlreadyExistsError
+)
 from shemas.user import (
     UserOutputShema, 
     UserCreateShema,
@@ -27,20 +34,38 @@ async def get_all() -> list[UserOutputShema]:
 
 @users_router.get('/by-id/{user_id}')
 async def get_one_by_id(user_id: Annotated[int, Path()]) -> UserOutputShema:
-    user = await user_service.get_one_by_id(id=user_id)
+    try:
+        user = await user_service.get_one_by_id(id=user_id)
+    except UserNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
 
     return user
 
 
 @users_router.get('/by-username/{username}')
 async def get_one_by_username(username: Annotated[str, Path()]) -> UserOutputShema:
-    user = await user_service.get_one_by_username(username=username)
+    try:
+        user = await user_service.get_one_by_username(username=username)
+    except UserNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
 
     return user
 
 @users_router.post('/create/')
 async def create_one(new_user: UserCreateShema):
-    new_user_id = await user_service.create_one(new_user=new_user)
+    try:
+        new_user_id = await user_service.create_one(new_user=new_user)
+    except UserAlreadyExistsError:
+        raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail='User with this username - already exists'
+        )
 
     return new_user_id
 
@@ -48,11 +73,28 @@ async def create_one(new_user: UserCreateShema):
 @users_router.patch('/update/{user_id}')
 async def update_one(user_id: Annotated[int, Path()], 
                      updated_user: UserUpgrateShema):
-    await user_service.update_one(user_id=user_id,
+    try:
+        await user_service.update_one(user_id=user_id,
                                   updated_user=updated_user)
+    except UserAlreadyExistsError:
+       raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail='User with this username - already exists'
+        )
+    except UserNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
 
 
 @users_router.delete('/delete/{user_id}')
 async def delete_one(user_id: Annotated[int, Path()]):
-    await user_service.delete_one(user_id=user_id)
+    try: 
+        await user_service.delete_one(user_id=user_id)
+    except UserNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
     
