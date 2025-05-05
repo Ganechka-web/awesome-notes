84a79d022e588d29dafc6ee246126b1b0566c5fc
Ganechka <maksim15848@gmail.com>
2025-05-05 22:54:22 +0300

    feat(exceptions): Exceptions for service layer

---
+++ notes_service/src/exceptions/service.py
@@ -0,0 +1,12 @@
+class ServiceError(Exception):
+    def __init__(self, msg: str,*args):
+        self.msg = msg
+        super().__init__(msg, *args)
+
+
+class NoteNotFoundError(ServiceError):
+    pass
+
+
+class NoteAlreadyExistsError(ServiceError):
+    pass
