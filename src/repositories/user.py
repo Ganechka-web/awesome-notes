06cb57bc28a5925ef059f12fcc93813b5d2e2748
Ganechka <maksim15848@gmail.com>
2025-05-05 09:27:48 +0300

    feat(repositories): exceptions handing

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy import select
 
+from exceptions.repositories import DataBaseError
 from models.user import User
 
 
@@ -22,30 +24,48 @@ class UserRepository:
             query = select(User).where(self.model.id == id)
             user = await session.execute(query)
 
-            return user.scalar_one_or_none()
+            try:
+                return user.scalar_one()
+            except NoResultFound as e:
+                raise DataBaseError(
+                    f'No such row id - {id}'
+                ) from e
 
     async def get_one_by_username(self, username: str) -> User:
         async with AsyncSession(self.engine) as session:
             query = select(self.model).where(self.model.username == username)
             user = await session.execute(query)
 
-            return user.scalar_one_or_none()      
+            try:
+                return user.scalar_one()
+            except NoResultFound as e:
+                raise DataBaseError(
+                    f'No such row username - {username}'
+                ) from e
 
     async def create_one(self, user: User) -> int:
         async with AsyncSession(self.engine) as session:
             session.add(user)
-            await session.flush()
+            try:
+                await session.flush()
+                new_user_id = user.id
+                await session.commit()
+            except IntegrityError as e:
+                raise DataBaseError(
+                    'Error during user saving'  
+                ) from e 
 
-            new_user_id = user.id
-
-            await session.commit()
-            
             return new_user_id
         
     async def update_one(self, user: User) -> None:
         async with AsyncSession(self.engine) as session:
             session.add(user)
-            await session.commit()
+            try:
+                await session.commit()
+            except IntegrityError as e:
+                raise DataBaseError(
+                    "Error during updating user"
+                ) from e
     
     async def delete_one(self, user: User) -> None:
         async with AsyncSession(self.engine) as session:
