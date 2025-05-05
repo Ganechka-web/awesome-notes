f1075fc8ba6ebb151f71b8f28adf1a90431c2ffa
Ganechka <maksim15848@gmail.com>
2025-05-05 10:44:08 +0300

    feat(shemas): user gender and age validations

--- src/shemas/user.py
+++ src/shemas/user.py
@@ -1,22 +1,33 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+
+from models.user import Gender
 
 
 class UserCreateShema(BaseModel):
     username: str = Field(max_length=50)
     gender: str
-    age: int
+    age: int = Field(ge=18, le=110)
 
     class Config:
         from_attributes = True
+    
+    @field_validator('gender')
+    @classmethod
+    def validate_user_gender(cls, gender: str) -> str:
+        genders = (Gender.male.value, Gender.female.value, 
+                   Gender.unknown.value)
+        if gender not in genders:
+            raise ValueError(f'Gender can be only {', '.join(genders)}')
+        return gender
 
 
 class UserUpgrateShema(UserCreateShema):
     username: Optional[str] = Field(default=None, max_length=50)
     gender: Optional[str] = None
-    age: Optional[int] = None
+    age: Optional[int] = Field(default=None, ge=18, le=110)
 
 
 class UserOutputShema(UserCreateShema):
