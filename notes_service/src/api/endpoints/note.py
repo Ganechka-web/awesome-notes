3171be8fb6c927cb41a4d5ad5de6e71a2948d172
Ganechka <maksim15848@gmail.com>
2025-05-06 23:42:01 +0300

    feat(api.endpoints): update_one, delete_one handlers

--- notes_service/src/api/endpoints/note.py
+++ notes_service/src/api/endpoints/note.py
@@ -5,7 +5,11 @@ from fastapi import APIRouter, Path, HTTPException, status
 from core.database import async_engine
 from repositories.note import NoteRepository
 from services.note import NoteService
-from shemas.note import NoteOutputShema, NoteCreateShema
+from shemas.note import (
+    NoteOutputShema, 
+    NoteCreateShema,
+    NoteUpdateShema
+)
 from exceptions.service import NoteNotFoundError, NoteAlreadyExistsError
 
 
@@ -56,3 +60,35 @@ async def create_one(new_note: NoteCreateShema) -> int:
         )
     
     return new_note_id
+
+
+@notes_router.patch('/update/{note_id}')
+async def update_one(
+    note_id: Annotated[int, Path()], updated_note: NoteUpdateShema
+):
+    try: 
+        await note_service.update_one(
+            note_id=note_id, updated_note=updated_note
+        )
+    except NoteNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='Note not found'
+        )
+    except NoteAlreadyExistsError:
+        raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail=f'User already has got a'
+                   f'note with title - {updated_note.title}'
+        )
+    
+
+@notes_router.delete('/delete/{note_id}')
+async def delete_one(note_id: Annotated[int, Path()]):
+    try:
+        await note_service.delete_one(note_id=note_id)
+    except NoteNotFoundError:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail='Note not found'
+        )
