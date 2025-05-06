ff6d4c9f2a58e6f4db75d3ea211e80ac3a1f1d99
Ganechka <maksim15848@gmail.com>
2025-05-06 23:40:59 +0300

    feat(services): update_one, delete_one methods

--- notes_service/src/services/note.py
+++ notes_service/src/services/note.py
@@ -3,7 +3,8 @@ from repositories.specifications import NotesForOwnerSpecification
 from models.note import Note
 from shemas.note import ( 
     NoteOutputShema, 
-    NoteCreateShema
+    NoteCreateShema,
+    NoteUpdateShema
 )
 from services.validators import NoteTitleUniqueForOwnerValidator
 from exceptions.repository import NoSuchRowError
@@ -52,3 +53,36 @@ class NoteService:
         new_note_id = await self.repository.create_one(note=new_note_orm)
 
         return new_note_id
+    
+    async def update_one(self, note_id: int, updated_note: NoteUpdateShema) -> None:
+        # check note existence
+        try: 
+            current_note = await self.repository.get_one_by_id(note_id=note_id)
+        except NoSuchRowError as e:
+            raise NoteNotFoundError(
+                f'Unable to find note with id - {note_id}'
+            ) from e
+        
+        note_title_unique_for_owner_validator = self.note_title_unique_for_owner_validator(
+            repository=self.repository, 
+            owner_id=Note.owner_id
+        )
+        await note_title_unique_for_owner_validator.validate(
+            new_note_title=updated_note.title
+        )
+
+        for field, value in updated_note.model_dump(exclude_unset=True).items():
+            setattr(current_note, field, value)
+
+        await self.repository.update_one(note=current_note)
+
+    async def delete_one(self, note_id: int) -> None:
+        # check note existence
+        try:
+            note_on_delete = await self.repository.get_one_by_id(note_id=note_id)
+        except NoSuchRowError as e:
+            raise NoteNotFoundError(
+                f'Unable to find note with id - {note_id}'
+            ) from e
+
+        await self.repository.delete_one(note=note_on_delete)
