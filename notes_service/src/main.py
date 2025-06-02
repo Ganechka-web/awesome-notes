18ceff0c1a7ceb37db05805a7c68e80d190a7c5e
Ganechka <maksim15848@gmail.com>
2025-06-02 11:20:55 +0300

    feat(main.py): add lifespan with connection and closing connection to broker

--- notes_service/src/main.py
+++ notes_service/src/main.py
@@ -1,8 +1,22 @@
+import asyncio 
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from api.endpoints.note import notes_router
+from broker.connection import (
+    connect_to_broker,
+    close_connection_to_broker
+)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(connect_to_broker())
+    yield
+    await asyncio.create_task(close_connection_to_broker()) 
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(notes_router)
