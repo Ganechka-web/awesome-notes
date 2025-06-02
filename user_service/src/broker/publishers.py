ec286352b48f067e3db15ff842a4416b79e0a199
Ganechka <maksim15848@gmail.com>
2025-06-02 10:57:26 +0300

    feat(broker): NotePublisher

---
+++ user_service/src/broker/publishers.py
@@ -0,0 +1,44 @@
+from typing import Self
+
+from aio_pika import Message
+from aio_pika.abc import AbstractConnection, AbstractChannel
+
+from core.settings import DELETE_NOTES_QUEUE_NAME
+from broker.connection import close_connection_to_broker
+
+
+class NotePublisher:
+    """
+    NotePublisher is an context manager to publish message to delete user`s notes.
+    It receives ready connection and after meeeage publishing closes connection. 
+    """
+    def __init__(self, connection: AbstractConnection) -> None:
+        self.connection = connection
+        self.channel: AbstractChannel = None
+
+    async def __aenter__(self) -> Self:
+        return self
+    
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        if exc_type:
+            # TO DO: implement logging
+            print('During message publishing raises an exeption ' 
+                  f'type - {exc_type}, value - {exc_value} '
+                  f'traceback - {traceback}')
+        await close_connection_to_broker()
+
+    async def _get_channel(self) -> AbstractChannel:
+        if not self.channel or self.channel.is_closed:
+            self.channel = await self.connection.channel()
+            await self.channel.declare_queue(
+                DELETE_NOTES_QUEUE_NAME, 
+                durable=True
+            )
+        return self.channel
+    
+    async def publish(self, data: bytes) -> None:
+        channel = await self._get_channel()
+        await channel.default_exchange.publish(
+            message=Message(data),
+            routing_key=DELETE_NOTES_QUEUE_NAME
+        )
