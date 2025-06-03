cb714858ea890ab5445f22dcbec373a0c5c86a2c
Ganechka <maksim15848@gmail.com>
2025-06-03 11:22:50 +0300

    improve(broker): refactor connection functions, logging

--- notes_service/src/broker/callbacks.py
+++ notes_service/src/broker/callbacks.py
@@ -3,6 +3,7 @@ import json
 from aio_pika.abc import AbstractIncomingMessage
 
 from api.endpoints.note import note_service
+from logger import logger
 
 
 async def delete_all_user_notes_handler(
@@ -10,5 +11,10 @@ async def delete_all_user_notes_handler(
 ) -> None:
     """Callback for receiving measseges to delete user`s notes."""
     data = json.loads(message.body)
+
+    logger.info(f"Received messege on delete user`s notes, user_id = {data['user_id']}")
+
     await note_service.delete_all_by_owner_id(owner_id=data['user_id'])
     await message.ack()
+
+    logger.info("Message has processed successfully")
--- notes_service/src/broker/connection.py
+++ notes_service/src/broker/connection.py
@@ -1,22 +1,25 @@
 from aio_pika import connect
 from aio_pika.abc import AbstractConnection
+from aio_pika.exceptions import AMQPConnectionError
 
 from core.settings import rabbitmq_settings, DELETE_NOTES_QUEUE_NAME
 from broker.callbacks import delete_all_user_notes_handler
+from logger import logger
 
 
-connection: AbstractConnection
-
-
-async def connect_to_broker() -> None:
-    """Connects to nessage broker and starts consuming"""
-    global connection
-    connection = await connect(
-        host=rabbitmq_settings.host,
-        port=rabbitmq_settings.port,
-        login=rabbitmq_settings.user,
-        password=rabbitmq_settings.password
-    )
+async def connect_to_broker() -> AbstractConnection:
+    """Connects to message broker and starts consuming"""
+    try:
+        connection = await connect(
+            host=rabbitmq_settings.host,
+            port=rabbitmq_settings.port,
+            login=rabbitmq_settings.user,
+            password=rabbitmq_settings.password
+        )
+        logger.info("Message broker has connected successfully")
+    except (AMQPConnectionError, ConnectionRefusedError):
+        logger.critical("Connection to message broker failed")
+        return
 
     # declare channel and queue
     channel = await connection.channel()
@@ -25,11 +28,18 @@ async def connect_to_broker() -> None:
         durable=True
     )
 
-    # srarts consuming
+    logger.info("Starting consuming...")
+
+    # starts consuming
     await delete_existing_notes_queue.consume(callback=delete_all_user_notes_handler)
 
+    return connection
 
-async def close_connection_to_broker() -> None:
-    global connection
-    if not connection.is_closed:
-        await connection.close()
+
+async def close_connection_to_broker(connection: AbstractConnection | None) -> None:
+    if not connection or connection.is_closed:
+        logger.critical("Message broker hasn`t been connected, shutting down failed")
+        return 
+    
+    await connection.close()
+    logger.info("Message broker has shut down successfully")
