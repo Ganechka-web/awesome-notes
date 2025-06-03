78d713bfa1a87cb6fabf4f2c2da4d01af9adf7d8
Ganechka <maksim15848@gmail.com>
2025-06-03 12:01:15 +0300

    feat(main.py): lifespan, direct uvicorn startup

--- user_service/src/main.py
+++ user_service/src/main.py
@@ -1,8 +1,23 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+import uvicorn
 from fastapi import FastAPI
 
+from core.database import main as healthcheck
 from api.endpoints.user import users_router
 
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(healthcheck())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(users_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
