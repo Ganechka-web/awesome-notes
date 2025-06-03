db3b5b16f7fb0967a04a7a84bcc5df8330783a75
Ganechka <maksim15848@gmail.com>
2025-06-03 11:21:13 +0300

    improve(main.py): add healthcheck to lifespan, direct uvicorn startup

--- notes_service/src/main.py
+++ notes_service/src/main.py
@@ -1,9 +1,11 @@
 import asyncio 
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI
 
 from api.endpoints.note import notes_router
+from core.database import main as healthcheck
 from broker.connection import (
     connect_to_broker,
     close_connection_to_broker
@@ -12,11 +14,17 @@ from broker.connection import (
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    asyncio.create_task(connect_to_broker())
+    connection = asyncio.create_task(connect_to_broker())
+    asyncio.create_task(healthcheck())
     yield
-    await asyncio.create_task(close_connection_to_broker()) 
+    await asyncio.create_task(
+        close_connection_to_broker(await connection)
+    )
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(notes_router)
+
+if __name__ == '__main__':
+     uvicorn.run('main:app', port=8001, reload=True)
