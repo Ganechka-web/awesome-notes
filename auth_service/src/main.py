b061e6d0b55517f94d23af80bf06b69beff7059d
Ganechka <maksim15848@gmail.com>
2025-06-04 19:03:27 +0300

    feat(main.py): lifespan, add healthcheck to lifespan

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -1,8 +1,19 @@
+import asyncio
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from core.database import main as healthcheck
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await asyncio.create_task(healthcheck())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8002, reload=True)
