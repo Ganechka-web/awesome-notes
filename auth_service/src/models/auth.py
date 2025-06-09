2f50a5cb9f5976fc89bc1986e8c2dcc4605253cf
Ganechka <maksim15848@gmail.com>
2025-06-09 09:38:37 +0300

    feat(models): auth_credentials model

---
+++ auth_service/src/models/auth.py
@@ -0,0 +1,19 @@
+from uuid import UUID, uuid4
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, UUID as SQL_UUID
+
+from core.database import Base
+
+
+class AuthCredentials(Base):
+    __tablename__ = "auth_credentials"
+    
+    id: Mapped[UUID] = mapped_column(SQL_UUID,
+                                     primary_key=True,
+                                     default=uuid4)
+    login: Mapped[str] = mapped_column(String(20), 
+                                       unique=True,
+                                       index=True)
+    password: Mapped[str]
+    
