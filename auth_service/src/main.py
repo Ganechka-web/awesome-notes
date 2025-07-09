eeeae733c064a00f3d9a53a453730e056c5b113a
Ganechka <maksim15848@gmail.com>
2025-07-09 12:00:51 +0300

    feat(main.py): add auth router

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.database import main as healthcheck
+from api.endpoints.auth import auth_router
 
 
 @asynccontextmanager
@@ -14,6 +15,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8002, reload=True)
