f3bb9c45758459b110d4045e80650c16066b11e0
Ganechka <maksim15848@gmail.com>
2025-07-09 12:25:24 +0300

    feat(main.py): dependency injection container

--- user_service/src/main.py
+++ user_service/src/main.py
@@ -1,18 +1,29 @@
-import asyncio
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
-from core.database import main as healthcheck
+from container import Container
+from core.settings import postgres_settings, rabbitmq_settings, USER_CREATION_QUEUE_NAME
 from api.endpoints.user import users_router
-
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    asyncio.create_task(healthcheck())
+    dep_container = Container()
+    dep_container.config.from_dict(
+        {
+            "postgres_settings": postgres_settings.model_dump(),
+            "rabbitmq_settings": rabbitmq_settings.model_dump(),
+        }
+    )
+    app.container = dep_container
+    await app.container.user_broker().consume(
+        queue_name=USER_CREATION_QUEUE_NAME, callback=app.container.create_user_callback()
+    )
     yield
+    await app.container.user_broker().shutdown()
+    await app.container.user_database().shutdown()
 
 
 app = FastAPI(lifespan=lifespan)
