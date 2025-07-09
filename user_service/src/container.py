1c704723f867139d48b1cb6d2c884faeaeebefda
Ganechka <maksim15848@gmail.com>
2025-07-09 12:12:40 +0300

    feat(container.py): dependencies container

---
+++ user_service/src/container.py
@@ -0,0 +1,43 @@
+from dependency_injector import containers, providers
+
+from core.broker import AsyncBroker
+from core.database import AsyncDatabase
+from repositories.user import UserRepository
+from services.user import UserService
+from broker.callbacks import CreateUserCallback
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        modules=["api.endpoints.user", "broker.callbacks"]
+    )
+    config = providers.Configuration()
+
+    user_database = providers.Singleton(
+        AsyncDatabase,
+        host=config.postgres_settings.host,
+        port=config.postgres_settings.port,
+        username=config.postgres_settings.user,
+        password=config.postgres_settings.password,
+        db=config.postgres_settings.db,
+    )
+    user_repository = providers.Factory(
+        UserRepository,
+        database=user_database
+    )
+    user_broker = providers.Singleton(
+        AsyncBroker,
+        host=config.rabbitmq_settings.host,
+        port=config.rabbitmq_settings.port,
+        login=config.rabbitmq_settings.user,
+        password=config.rabbitmq_settings.password
+    )
+    user_service = providers.Factory(
+        UserService,
+        repository=user_repository,
+        broker=user_broker
+    )
+    create_user_callback = providers.Factory(
+        CreateUserCallback,
+        user_service=user_service
+    )
