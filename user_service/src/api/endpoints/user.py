c0105943e7436ee0787333b38841475d0318b163
Ganechka <maksim15848@gmail.com>
2025-07-09 12:24:33 +0300

    refactor(api.endpoints): dependency injection, formating

--- user_service/src/api/endpoints/user.py
+++ user_service/src/api/endpoints/user.py
@@ -1,93 +1,86 @@
 from typing import Annotated
+from uuid import UUID
 
-from fastapi import (
-    APIRouter, Path, 
-    HTTPException, status
-)
+from fastapi import APIRouter, Path, Depends, HTTPException, status
+from dependency_injector.wiring import Provide, inject
 
-from core.database import async_engine
-from repositories.user import UserRepository
+from container import Container
+from exceptions.broker import UnableToConnectToBrokerError
+from exceptions.services import UserNotFoundError, UserAlreadyExistsError
 from services.user import UserService
-from exceptions.services import (
-    UserNotFoundError,
-    UserAlreadyExistsError
-)
-from exceptions.broker import PublisherCantConnectToBrokerError
-from shemas.user import (
-    UserOutputShema, 
-    UserUpgrateShema
-)
+from shemas.user import UserOutputShema, UserUpgrateShema
 
 
-users_router = APIRouter(prefix='/users')
-
-user_repository = UserRepository(async_engine)
-user_service = UserService(user_repository)
+users_router = APIRouter(prefix="/users")
 
 
-@users_router.get('/')
-async def get_all() -> list[UserOutputShema]:
+@users_router.get("/")
+@inject
+async def get_all(
+    user_service: UserService = Depends(Provide[Container.user_service]),
+) -> list[UserOutputShema]:
     users = await user_service.get_all()
 
     return users
 
 
-@users_router.get('/by-id/{user_id}')
-async def get_one_by_id(user_id: Annotated[int, Path()]) -> UserOutputShema:
+@users_router.get("/by-id/{user_id}")
+@inject
+async def get_one_by_id(
+    user_id: Annotated[UUID, Path()],
+    user_service: UserService = Depends(Provide[Container.user_service]),
+) -> UserOutputShema:
     try:
         user = await user_service.get_one_by_id(id=user_id)
     except UserNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='User not found'
-        )
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
 
     return user
 
 
-@users_router.get('/by-username/{username}')
-async def get_one_by_username(username: Annotated[str, Path()]) -> UserOutputShema:
+@users_router.get("/by-username/{username}")
+@inject
+async def get_one_by_username(
+    username: Annotated[str, Path()],
+    user_service: UserService = Depends(Provide[Container.user_service]),
+) -> UserOutputShema:
     try:
         user = await user_service.get_one_by_username(username=username)
     except UserNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='User not found'
-        )
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
 
     return user
 
 
-@users_router.patch('/update/{user_id}')
-async def update_one(user_id: Annotated[int, Path()], 
-                     updated_user: UserUpgrateShema):
+@users_router.patch("/update/{user_id}")
+@inject
+async def update_one(
+    user_id: Annotated[UUID, Path()],
+    updated_user: UserUpgrateShema,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
     try:
-        await user_service.update_one(user_id=user_id,
-                                      updated_user=updated_user)
+        await user_service.update_one(user_id=user_id, updated_user=updated_user)
     except UserAlreadyExistsError:
-       raise HTTPException(
-            status.HTTP_409_CONFLICT,
-            detail='User with this username - already exists'
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, detail="User with this username - already exists"
         )
     except UserNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='User not found'
-        )
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@users_router.delete('/delete/{user_id}')
-async def delete_one(user_id: Annotated[int, Path()]):
-    try: 
+@users_router.delete("/delete/{user_id}")
+@inject
+async def delete_one(
+    user_id: Annotated[UUID, Path()],
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
+    try:
         await user_service.delete_one(user_id=user_id)
     except UserNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='User not found'
-        )
-    except PublisherCantConnectToBrokerError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+    except UnableToConnectToBrokerError:
         raise HTTPException(
             status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail='Unable to delete user, please try again later'
+            detail="Unable to delete user, please try again later",
         )
-    
