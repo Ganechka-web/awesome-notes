26a4127c1a4c48ef0ad9ef6e7f54201320c1bccf
Ganechka <maksim15848@gmail.com>
2025-07-09 11:51:16 +0300

    feat(schemas): User schemas, edit login schema

--- auth_service/src/schemas/auth.py
+++ auth_service/src/schemas/auth.py
@@ -1,17 +1,50 @@
-from pydantic import BaseModel
+from uuid import UUID
+
+from pydantic import BaseModel, Field, field_validator, field_serializer
+
+
+class UserEventSchema(BaseModel):
+    username: str = Field(max_length=50)
+    gender: str
+    age: int = Field(ge=18, le=110)
+
+    class Config:
+        from_attributes = True
+    
+    @field_validator('gender')
+    @classmethod
+    def validate_user_gender(cls, gender: str) -> str:
+        genders = ('male', 'female', 
+                   'unknown')
+        if gender not in genders:
+            raise ValueError(f'Gender can be only {', '.join(genders)}')
+        return gender
+    
+
+class UserEventCreateSchema(UserEventSchema):
+    id: UUID
+
+    @field_serializer('id')
+    def serialize_id(self, id: UUID, _info) -> str:
+        return str(id)
 
 
 class AuthCredentialsRegisterSchema(BaseModel):
     login: str
     password: str
+    user_data: UserEventSchema
 
     class Config:
         from_attributes = True
 
 
-class AuthCredentialsLoginSchema(AuthCredentialsRegisterSchema):
-    pass
+class AuthCredentialsLoginSchema(BaseModel):
+    login: str
+    password: str
+    
+    class Config:
+        from_attributes = True
 
 
-class AuthCredentialsSchema(AuthCredentialsRegisterSchema):
+class AuthCredentialsSchema(AuthCredentialsLoginSchema):
     id: int
