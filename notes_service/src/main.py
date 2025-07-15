eab8079831edc12c5e4dfef6549e0266bb429195
Ganechka <maksim15848@gmail.com>
2025-07-15 20:30:17 +0300

    feat(main.py): add dependencies container to lifespan, formatting

--- notes_service/src/main.py
+++ notes_service/src/main.py
@@ -1,30 +1,34 @@
-import asyncio 
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
 from api.endpoints.note import notes_router
-from core.database import main as healthcheck
-from broker.connection import (
-    connect_to_broker,
-    close_connection_to_broker
-)
+from core.settings import postgres_settings, rabbitmq_settings, DELETE_NOTES_QUEUE_NAME
+from container import Container
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    connection = asyncio.create_task(connect_to_broker())
-    asyncio.create_task(healthcheck())
-    yield
-    await asyncio.create_task(
-        close_connection_to_broker(await connection)
+    dep_container = Container()
+    dep_container.config.from_dict(
+        {
+            "postgres_settings": postgres_settings.model_dump(),
+            "rabbitmq_settings": rabbitmq_settings.model_dump(),
+        }
     )
+    app.container = dep_container
+    await app.container.note_broker().consume(
+        queue_name=DELETE_NOTES_QUEUE_NAME,
+        callback=app.container.delete_all_user_notes_callback(),
+    )
+    yield
+    await app.container.note_broker().shutdown()
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(notes_router)
 
-if __name__ == '__main__':
-     uvicorn.run('main:app', port=8001, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8001, reload=True)
