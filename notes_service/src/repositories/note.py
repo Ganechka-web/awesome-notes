485f0168ebb83d1da29d7e4ce5441d57021df91e
Ganechka <maksim15848@gmail.com>
2025-07-15 20:08:26 +0300

    feat(repositories): new db managing usage, switch id types to UUID

--- notes_service/src/repositories/note.py
+++ notes_service/src/repositories/note.py
@@ -1,4 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from typing import TYPE_CHECKING
+from uuid import UUID
+
 from sqlalchemy.exc import NoResultFound, IntegrityError
 from sqlalchemy import select, delete
 
@@ -6,40 +8,41 @@ from models.note import Note
 from exceptions.repository import DatabaseError, NoSuchRowError
 from repositories.specifications import Specification
 
+if TYPE_CHECKING:
+    from core.database import AsyncDatabase
+
 
 class NoteRepository:
     model = Note
 
-    def __init__(self, engine: AsyncEngine):
-        self.engine = engine
+    def __init__(self, database: "AsyncDatabase"):
+        self.db = database
 
     async def get_all(self) -> list[Note]:
-        async with AsyncSession(self.engine) as session:
+        async with self.db.get_session() as session:
             query = select(self.model)
             notes = await session.scalars(query)
 
             return notes.all()
-        
-    async def filter_by(self, specification: Specification) -> list[Note]: 
-        async with AsyncSession(self.engine) as session:
+
+    async def filter_by(self, specification: Specification) -> list[Note]:
+        async with self.db.get_session() as session:
             query = select(self.model).where(*specification.is_satisfied())
             filtered_notes = await session.scalars(query)
 
             return filtered_notes.all()
-    
-    async def get_one_by_id(self, note_id: int) -> Note:
-        async with AsyncSession(self.engine) as session:
+
+    async def get_one_by_id(self, note_id: UUID) -> Note:
+        async with self.db.get_session() as session:
             query = select(self.model).where(self.model.id == note_id)
             note = await session.execute(query)
             try:
                 return note.scalar_one()
             except NoResultFound as e:
-                raise NoSuchRowError(
-                    f'Unable to find row with id - {note_id}'
-                ) from e
-            
+                raise NoSuchRowError(f"Unable to find row with id - {note_id}") from e
+
     async def create_one(self, note: Note) -> int:
-        async with AsyncSession(self.engine) as session:
+        async with self.db.get_session() as session:
             session.add(note)
 
             try:
@@ -48,31 +51,27 @@ class NoteRepository:
                 await session.commit()
             except IntegrityError as e:
                 await session.rollback()
-                raise DatabaseError(
-                    'Error during saving row'
-                ) from e
+                raise DatabaseError("Error during saving row") from e
 
-            return new_note_id    
+            return new_note_id
 
     async def update_one(self, note: Note) -> None:
-        async with AsyncSession(self.engine) as session:
+        async with self.db.get_session() as session:
             session.add(note)
 
-            try: 
+            try:
                 await session.commit()
             except IntegrityError as e:
                 await session.rollback()
-                raise DatabaseError(
-                    'Error during saving row'
-                ) from e
-    
+                raise DatabaseError("Error during saving row") from e
+
     async def delete_one(self, note: Note) -> None:
-        async with AsyncSession(self.engine) as session:
+        async with self.db.get_session() as session:
             await session.delete(note)
             await session.commit()
-    
-    async def delete_all(self, note_ids: list[int]) -> None:
-        async with AsyncSession(self.engine) as session:
+
+    async def delete_all(self, note_ids: list[UUID]) -> None:
+        async with self.db.get_session() as session:
             query = delete(self.model).where(self.model.id.in_(note_ids))
             await session.execute(query)
             await session.commit()
