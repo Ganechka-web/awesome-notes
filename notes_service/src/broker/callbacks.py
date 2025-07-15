f81a6ec47c837730ebf1e0d4eb5a4afecb526ac8
Ganechka <maksim15848@gmail.com>
2025-07-15 20:25:39 +0300

    refactor(broker): callbacks as classes

--- notes_service/src/broker/callbacks.py
+++ notes_service/src/broker/callbacks.py
@@ -1,20 +1,32 @@
 import json
+from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
 
 from aio_pika.abc import AbstractIncomingMessage
 
-from api.endpoints.note import note_service
 from logger import logger
 
+if TYPE_CHECKING:
+    from services.note import NoteService
 
-async def delete_all_user_notes_handler(
-    message: AbstractIncomingMessage
-) -> None:
-    """Callback for receiving measseges to delete user`s notes."""
-    data = json.loads(message.body)
 
-    logger.info(f"Received messege on delete user`s notes, user_id = {data['user_id']}")
+class BaseCallback(ABC):
+    @abstractmethod
+    async def handle(self, message: AbstractIncomingMessage):
+        raise NotImplementedError
 
-    await note_service.delete_all_by_owner_id(owner_id=data['user_id'])
-    await message.ack()
 
-    logger.info("Message has processed successfully")
+class DeleteAllUserNotesCallback(BaseCallback):
+    """Callback for receiving messages to delete user`s notes"""
+
+    def __init__(self, note_service: "NoteService") -> None:
+        self.note_service = note_service
+
+    async def handle(self, message: AbstractIncomingMessage) -> None:
+        logger.info("Received messege on delete user`s notes")
+        user_id = json.loads(message.body)["user_id"]
+
+        await self.note_service.delete_all_by_owner_id(owner_id=user_id)
+        await message.ack()
+
+        logger.info("Message has processed successfully")
