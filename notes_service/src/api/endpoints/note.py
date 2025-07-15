287498df4eb1bdf6b7a5e74f87ed241f84cc707e
Ganechka <maksim15848@gmail.com>
2025-07-15 20:22:24 +0300

    feat(api.endpoints): dependencies injection, switch id, owner_id to UUID, formatting

--- notes_service/src/api/endpoints/note.py
+++ notes_service/src/api/endpoints/note.py
@@ -1,35 +1,37 @@
-from typing import Annotated
+from typing import Annotated, TYPE_CHECKING
+from uuid import UUID
 
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, Depends, status
+from dependency_injector.wiring import Provide, inject
 
-from core.database import async_engine
-from repositories.note import NoteRepository
-from services.note import NoteService
-from shemas.note import (
-    NoteOutputShema, 
-    NoteCreateShema,
-    NoteUpdateShema
-)
+from shemas.note import NoteOutputShema, NoteCreateShema, NoteUpdateShema
 from exceptions.service import NoteNotFoundError, NoteAlreadyExistsError
+from container import Container
+
+if TYPE_CHECKING:
+    from services.note import NoteService
 
 
 notes_router = APIRouter(prefix="/notes")
 
-note_repository = NoteRepository(async_engine)
-note_service = NoteService(note_repository)
-
 
 @notes_router.get("/")
-async def get_all(md_content_format: bool = False) -> list[NoteOutputShema]:
+@inject
+async def get_all(
+    md_content_format: bool = False,
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
+) -> list[NoteOutputShema]:
     notes = await note_service.get_all(md_content_format=md_content_format)
 
     return notes
 
 
 @notes_router.get("/by-owner-id/{owner_id}")
+@inject
 async def get_all_by_owner_id(
-    owner_id: Annotated[int, Path()],
-    md_content_format: bool = False
+    owner_id: Annotated[UUID, Path()],
+    md_content_format: bool = False,
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
 ) -> list[NoteOutputShema]:
     notes_by_owner_id = await note_service.get_all_by_owner_id(
         owner_id=owner_id, md_content_format=md_content_format
@@ -38,73 +40,74 @@ async def get_all_by_owner_id(
     return notes_by_owner_id
 
 
-@notes_router.get('/by-id/{note_id}')
+@notes_router.get("/by-id/{note_id}")
+@inject
 async def get_one_by_id(
-    note_id: Annotated[int, Path()],
-    md_content_format: bool = False
+    note_id: Annotated[UUID, Path()],
+    md_content_format: bool = False,
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
 ) -> NoteOutputShema:
     try:
         note = await note_service.get_one_by_id(
             note_id=note_id, md_content_format=md_content_format
         )
     except NoteNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='Note not found'
-        )
-    
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Note not found")
+
     return note
 
 
-@notes_router.post('/create/')
-async def create_one(new_note: NoteCreateShema) -> int:
+@notes_router.post("/create/")
+@inject
+async def create_one(
+    new_note: NoteCreateShema,
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
+) -> UUID:
     try:
         new_note_id = await note_service.create_one(new_note=new_note)
-    except NoteAlreadyExistsError: 
-        raise HTTPException(
-            status.HTTP_409_CONFLICT,
-            detail=f'User already has got a'
-                   f'note with title - {new_note.title}'
-        )
-    
-    return new_note_id
-
-
-@notes_router.patch('/update/{note_id}')
-async def update_one(
-    note_id: Annotated[int, Path()], updated_note: NoteUpdateShema
-):
-    try: 
-        await note_service.update_one(
-            note_id=note_id, updated_note=updated_note
-        )
-    except NoteNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='Note not found'
-        )
     except NoteAlreadyExistsError:
         raise HTTPException(
             status.HTTP_409_CONFLICT,
-            detail=f'User already has got a'
-                   f'note with title - {updated_note.title}'
+            detail=f"User already has got anote with title - {new_note.title}",
         )
-    
 
-@notes_router.delete('/delete/{note_id}')
-async def delete_one(note_id: Annotated[int, Path()]):
+    return new_note_id
+
+
+@notes_router.patch("/update/{note_id}")
+@inject
+async def update_one(
+    note_id: Annotated[UUID, Path()],
+    updated_note: NoteUpdateShema,
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
+) -> None:
+    try:
+        await note_service.update_one(note_id=note_id, updated_note=updated_note)
+    except NoteNotFoundError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Note not found")
+    except NoteAlreadyExistsError:
+        raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail=f"User already has got anote with title - {updated_note.title}",
+        )
+
+
+@notes_router.delete("/delete/{note_id}")
+@inject
+async def delete_one(
+    note_id: Annotated[UUID, Path()],
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
+) -> None:
     try:
         await note_service.delete_one(note_id=note_id)
     except NoteNotFoundError:
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='Note not found'
-        )
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Note not found")
 
 
-@notes_router.delete('/delete/by-owner-id/{owner_id}')
+@notes_router.delete("/delete/by-owner-id/{owner_id}")
+@inject
 async def delete_all_by_owner_id(
-    owner_id: Annotated[int, Path()]
+    owner_id: Annotated[UUID, Path()],
+    note_service: "NoteService" = Depends(Provide[Container.note_service]),
 ) -> None:
     await note_service.delete_all_by_owner_id(owner_id=owner_id)
-    
