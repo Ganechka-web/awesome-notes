b4aa12861071182c5a0c697f14a08c68fa048333
Ganechka <maksim15848@gmail.com>
2025-07-15 20:16:51 +0300

    fix(schemas): switch id, owner_id types to UUID

--- notes_service/src/shemas/note.py
+++ notes_service/src/shemas/note.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from uuid import UUID
 from datetime import datetime
 
 from pydantic import BaseModel
@@ -7,7 +8,7 @@ from pydantic import BaseModel
 class NoteCreateShema(BaseModel):
     title: str
     content: str
-    owner_id: int
+    owner_id: UUID
 
     class Config:
         from_attributes = True
@@ -16,10 +17,10 @@ class NoteCreateShema(BaseModel):
 class NoteUpdateShema(NoteCreateShema):
     title: Optional[str] = None
     content: Optional[str] = None
-    owner_id: Optional[int] = None
+    owner_id: Optional[UUID] = None
 
 
 class NoteOutputShema(NoteCreateShema):
-    id: int
+    id: UUID
     created_at: datetime
     updated_at: datetime
