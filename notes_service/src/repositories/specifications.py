029a9796e8f465c564351581114ca835609c5213
Ganechka <maksim15848@gmail.com>
2025-07-15 20:15:40 +0300

    fix(repositories): switch id type to UUID, formatting

--- notes_service/src/repositories/specifications.py
+++ notes_service/src/repositories/specifications.py
@@ -1,5 +1,6 @@
 # Contains NoteRepository specifications
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from models.note import Note
 
@@ -12,11 +13,9 @@ class Specification(ABC):
 
 class NotesForOwnerSpecification(Specification):
     """specification for filter notes by owner_id"""
-    def __init__(self, owner_id: int) -> None:
+
+    def __init__(self, owner_id: UUID) -> None:
         self.owner_id = owner_id
 
-
     def is_satisfied(self) -> tuple:
-        return (
-            Note.owner_id == self.owner_id,
-        )
+        return (Note.owner_id == self.owner_id,)
