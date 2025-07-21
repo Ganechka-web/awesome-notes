e45306f9d67e5209609235a4bf10c67beffb4513
Ganechka <maksim15848@gmail.com>
2025-07-21 09:20:37 +0300

    feat(services): RPC client usage, refactoring, formatting

--- auth_service/src/services/auth.py
+++ auth_service/src/services/auth.py
@@ -1,83 +1,92 @@
 import json
+from uuid import UUID
+from typing import TYPE_CHECKING
 
 from schemas.auth import (
     AuthCredentialsSchema,
     AuthCredentialsRegisterSchema,
-    AuthCredentialsLoginSchema
+    AuthCredentialsLoginSchema,
 )
 from models.auth import AuthCredentials
-from repositories.auth import AuthRepository
 from exceptions.repositories import RowDoesNotExist
 from exceptions.services import (
-    AuthCredentialsNotFoundError, 
+    AuthCredentialsNotFoundError,
     AuthCredentialsAlreadyExistsError,
-    PasswordsDidNotMatch
+    PasswordsDidNotMatch,
+    UnableToCreareAuthCredentials,
 )
 from exceptions.broker import (
-    PublisherCantConnectToBrokerError,
-    PublisherTimeoutReceivingResponseError
+    UnableToConnectToBrokerError,
+    ReceivingResponseTimeOutError,
 )
 from security.passwords import get_password_hash, check_password_hash
 from security.jwt import get_jwt_token
-from broker.publishers import UserCreationPublisher
+
+if TYPE_CHECKING:
+    from repositories.auth import AuthRepository
+    from broker.rpc_clients import RPCClient
 
 
 class AuthService:
-    def __init__(self, repository: AuthRepository):
+    def __init__(
+        self,
+        repository: "AuthRepository",
+        rpc_client: "RPCClient",
+        user_creation_queue_name: str,
+    ) -> None:
         self.repository = repository
+        self.rpc_client = rpc_client
+        self.user_creation_queue_name = user_creation_queue_name
 
     async def get_one_by_login(self, login: str) -> AuthCredentialsSchema:
         try:
-            credentials_orm = await self.repository.get_one_by_login(
-                login=login
-            )
+            credentials_orm = await self.repository.get_one_by_login(login=login)
         except RowDoesNotExist as e:
             raise AuthCredentialsNotFoundError(
-                f'AuthCredentials with login - {login}'
-                'does not exist'
+                f"AuthCredentials with login - {login} does not exist"
             ) from e
 
         return AuthCredentialsSchema.model_validate(credentials_orm)
-    
+
     async def register(self, credentials: AuthCredentialsRegisterSchema) -> str:
         # check credentials existence
-        common_id: int
+        common_id: UUID = 0  # rewrite
         try:
-            _ = await self.repository.get_one_by_login(
-                login=credentials.login
-            )
+            _ = await self.repository.get_one_by_login(login=credentials.login)
             raise AuthCredentialsAlreadyExistsError(
-                f'AuthCredentials with login - {credentials.login}'
-                'already exists'
+                f"AuthCredentials with login - {credentials.login} already exists"
             )
         except RowDoesNotExist:
-            # sending message on user creation
+            # call the RPC client to get created user id
             try:
-                async with UserCreationPublisher() as publisher:
-                    user_data = credentials.user_data
-                    data = bytes(
-                        json.dumps(user_data.model_dump()),
-                        encoding='utf-8'
-                    )
-                    
-                    common_id = await publisher.publish_and_get_created_user_id(data=data)
-            except (PublisherCantConnectToBrokerError,
-                    PublisherTimeoutReceivingResponseError):
-                raise 
+                data = json.dumps(credentials.user_data.model_dump()).encode()
+                response = await self.rpc_client.call(
+                    self.user_creation_queue_name, data
+                )
+                common_id = UUID(hex=json.loads(response)["created_user_id"])
+            except (
+                UnableToConnectToBrokerError,
+                ReceivingResponseTimeOutError,
+            ) as e:
+                raise UnableToCreareAuthCredentials(
+                    "Unable to create AuthCredentials, error on the broker side"
+                ) from e
 
             # creating and setting up password hash
             password_hash = get_password_hash(credentials.password)
             credentials.password = password_hash
 
-            auth_credentials = AuthCredentials(**credentials.model_dump(exclude="user_data"))
+            auth_credentials = AuthCredentials(
+                **credentials.model_dump(exclude="user_data")
+            )
             # setting up the common id
             auth_credentials.id = common_id
             new_credentials_id = await self.repository.create_one(
                 credentials=auth_credentials
             )
-            
+
             return new_credentials_id
-    
+
     async def login(self, credentials: AuthCredentialsLoginSchema) -> str:
         # check credentials existence
         try:
@@ -86,8 +95,7 @@ class AuthService:
             )
         except RowDoesNotExist as e:
             raise AuthCredentialsNotFoundError(
-                f'AuthCredentials with login - {credentials.login}'
-                'does not exist'
+                f"AuthCredentials with login - {credentials.login} does not exist"
             ) from e
 
         # check passwords
@@ -101,5 +109,5 @@ class AuthService:
             return access_token
         else:
             raise PasswordsDidNotMatch(
-                f'Invalid password for login - {credentials.login}'
+                f"Invalid password for login - {credentials.login}"
             )
