f822cd701629b3ae5cf4c394ac768f2541996015
Ganechka <maksim15848@gmail.com>
2025-08-18 10:44:33 +0300

    refactor(broker): new error format

--- user_service/src/broker/callbacks.py
+++ user_service/src/broker/callbacks.py
@@ -6,6 +6,7 @@ from aio_pika.abc import AbstractIncomingMessage, AbstractChannel
 from aio_pika import Message
 
 from shemas.user import UserCreateShema
+from shemas.integration_errors import UserServiceCreationErrorSchema
 from exceptions.services import UserAlreadyExistsError
 from logger import logger
 
@@ -25,20 +26,26 @@ class CreateUserCallback:
         logger.info("Received message on user creation")
 
         # create new user and get his id
-        new_user_id: UUID
+        new_user_id: UUID = None
+        error: UserServiceCreationErrorSchema = None
         try:
             user_schema = UserCreateShema.model_validate(json.loads(message.body))
             new_user_id = await self.user_service.create_one(new_user=user_schema)
-        except UserAlreadyExistsError:
-            raise
+        except UserAlreadyExistsError as err:
+            error = UserServiceCreationErrorSchema(message=err.msg)
 
-        # send message with user_id to reply queue
+        # send message with created_user_id to reply queue
         if message.reply_to:
             await channel.default_exchange.publish(
                 message=Message(
-                    body=json.dumps({"created_user_id": new_user_id.hex}).encode(
-                        encoding="utf-8"
-                    ),
+                    body=json.dumps(
+                        {
+                            "created_user_id": new_user_id.hex
+                            if new_user_id
+                            else new_user_id,
+                            "error": error.model_dump_json() if error else None,
+                        }
+                    ).encode(encoding="utf-8"),
                     correlation_id=message.correlation_id,
                 ),
                 routing_key=message.reply_to,
