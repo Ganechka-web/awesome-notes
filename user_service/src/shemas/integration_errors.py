754928e496ecf56f8e37ebcb1f60b559d4d8a50d
Ganechka <maksim15848@gmail.com>
2025-08-18 10:40:48 +0300

    feat(schemas): integration error schemas

---
+++ user_service/src/shemas/integration_errors.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class BaseServiceIntegrationErrorSchema(BaseModel):
+    """Base class for integration errors"""
+    service_name: str = "user_service"
+    http_status_code: int
+    message: str
+
+
+class UserServiceCreationErrorSchema(BaseServiceIntegrationErrorSchema):
+    """
+    UserServiceCreationErrorSchema uses when service
+    returns an error because of conflict with a new user
+    """
+    http_status_code: int = 409
+    
