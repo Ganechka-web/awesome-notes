8b5df6d16edefdf36f9581a612ba953deaa7001f
Ganechka <maksim15848@gmail.com>
2025-08-22 20:37:44 +0300

    fix(main.py): switch host to 0.0.0.0

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -28,4 +28,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8002, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
