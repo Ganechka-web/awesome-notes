07db3ad9006d3985e18d6222c65a79ce0a86aa8b
Ganechka <maksim15848@gmail.com>
2025-09-06 20:59:53 +0300

    feat(services): new passwords management

---
+++ auth_service/src/services/security.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+
+class SecurityPasswordService:
+    context = CryptContext(schemes=["bcrypt"])
+
+    def generate_password_hash(self, bare_password: str) -> str:
+        return self.context.hash(bare_password)
+
+    def verify_password_hash(self, bare_password: str, password_hash: str) -> bool:
+        return self.context.verify(bare_password, password_hash)
+    
