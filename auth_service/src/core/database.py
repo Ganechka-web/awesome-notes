d5e00967f5bc1a757b6455ce0d7db20b3fa7963a
Ganechka <maksim15848@gmail.com>
2025-09-06 20:51:04 +0300

    refactor(core): creating engine in __init__, absolute imports

--- auth_service/src/core/database.py
+++ auth_service/src/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engin
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import URL
 
-from logger import logger
+from src.logger import logger
 
 
 class AsyncDatabase:
@@ -19,18 +19,13 @@ class AsyncDatabase:
             password=password,
             database=db,
         )
-        self.async_engine: AsyncEngine | None = None
+        self.async_engine: AsyncEngine = create_async_engine(
+            self.postgres_dcn, echo=True
+        )
         self.session: AsyncSession | None = None
-
-    def _create_async_engine(self) -> None:
-        if self.async_engine is None:
-            self.async_engine = create_async_engine(self.postgres_dcn, echo=True)
-            logger.info("Database is connected and ready to execute queries")
+        logger.info("Database is connected and ready to execute queries")
 
     def get_session(self) -> AsyncSession:
-        if self.async_engine is None:
-            self._create_async_engine()
-
         if self.session is None:
             self.session = AsyncSession(bind=self.async_engine)
         return self.session
@@ -38,6 +33,8 @@ class AsyncDatabase:
     async def shutdown(self) -> None:
         if self.session:
             await self.session.aclose()
+        if self.async_engine:
+            await self.async_engine.dispose()
         logger.info("Database has shut down successfully")
 
 
