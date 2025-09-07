befb6bf92b4bdd33d0cec2ec609c52c33cb0cef7
Ganechka <maksim15848@gmail.com>
2025-09-07 09:41:49 +0300

    feat(main.py): app creating function, host

--- user_service/src/main.py
+++ user_service/src/main.py
@@ -4,20 +4,16 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.container import Container
-from src.core.settings import postgres_settings, rabbitmq_settings, USER_CREATION_QUEUE_NAME
+from src.core.settings import (
+    postgres_settings,
+    rabbitmq_settings,
+    USER_CREATION_QUEUE_NAME,
+)
 from src.api.endpoints.user import users_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    dep_container = Container()
-    dep_container.config.from_dict(
-        {
-            "postgres_settings": postgres_settings.model_dump(),
-            "rabbitmq_settings": rabbitmq_settings.model_dump(),
-        }
-    )
-    app.container = dep_container
     await app.container.user_broker().consume(
         queue_name=USER_CREATION_QUEUE_NAME,
         callback=app.container.create_user_callback(),
@@ -27,9 +23,20 @@ async def lifespan(app: FastAPI):
     await app.container.user_database().shutdown()
 
 
-app = FastAPI(title="User service", version="0.0.0 alpha", lifespan=lifespan)
+def create_app() -> FastAPI:
+    dep_container = Container()
+    dep_container.config.from_dict(
+        {
+            "postgres_settings": postgres_settings.model_dump(),
+            "rabbitmq_settings": rabbitmq_settings.model_dump(),
+        }
+    )
+    app = FastAPI(title="User service", version="0.0.0 alpha", lifespan=lifespan)
+    app.container = dep_container
 
+
+app = create_app()
 app.include_router(users_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
