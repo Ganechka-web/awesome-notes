8085aa750e4d9c6d4067ccdfb1e91d9cd135d02f
Ganechka-web <maksim15848@gmail.com>
2025-09-19 12:55:32 +0300

    feat(tests): global fixtures

--- auth_service/src/tests/conftest.py
+++ auth_service/src/tests/conftest.py
@@ -1,32 +1,73 @@
-from typing import TYPE_CHECKING
+import json
+from typing import TYPE_CHECKING, Generator, AsyncGenerator
 
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
+from testcontainers.postgres import PostgresContainer, DbContainer
 
 from src.main import app
-from src.tests.db.database import TestDatabase
+from src.models.auth import AuthCredentials
+from src.core.database import Base
 
 if TYPE_CHECKING:
-    from src.services.security import SecurityPasswordService
+    from src.core.database import AsyncDatabase
 
 
 @pytest.fixture(scope="session")
-def client():
+def client() -> TestClient:
     return TestClient(app=app)
 
 
 @pytest.fixture(scope="session")
-def get_password_service(client: TestClient) -> "SecurityPasswordService":
-    return client.app.container.password_service()
+def container(client):
+    return client.app.container
 
 
-@pytest_asyncio.fixture
-async def get_test_db():
-    db = TestDatabase() 
+@pytest.fixture(scope="session")
+def get_postgres_container(container) -> Generator[DbContainer, None, None]:
+    postgres_settings = container.config.get("postgres_settings")
+    postgres_container = PostgresContainer(
+        image="postgres:latest",
+        driver="asyncpg",
+        username=postgres_settings["user"],
+        password=postgres_settings["password"],
+        dbname=postgres_settings["db"],
+    ).start()
 
-    await db.create_test_tables()
-    yield db
-    await db.shutdown()
+    container.config.set(
+        "postgres_settings.host", postgres_container.get_container_host_ip()
+    )
+    container.config.set(
+        "postgres_settings.port", postgres_container.get_exposed_port(5432)
+    )
+    container.reset_singletons()
+
+    yield postgres_container
+    postgres_container.stop()
 
 
+@pytest_asyncio.fixture(scope="session")
+async def get_test_db(get_postgres_container, container) -> AsyncGenerator["AsyncDatabase", None]:
+    test_db = container.auth_database()
+    yield test_db
+    await test_db.shutdown()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def tables(get_test_db: "AsyncDatabase") -> AsyncGenerator[None, None]:
+    conn = await get_test_db.async_engine.connect()
+    await conn.run_sync(Base.metadata.create_all)
+    await conn.commit()
+    yield
+    await conn.run_sync(Base.metadata.drop_all)
+    await conn.aclose()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def insert_test_data(get_test_db: "AsyncDatabase", tables) -> None:
+    async with get_test_db.get_session() as session:
+        with open("src/tests/repositories/test_db_data.json", "r") as file:
+            entities_orm = (AuthCredentials(**entity) for entity in json.load(file))
+        session.add_all(entities_orm)
+        await session.commit()
