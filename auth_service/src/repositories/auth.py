b895b54f2216bc119a390e05b22bb556f6edba98
Ganechka-web <maksim15848@gmail.com>
2025-09-19 12:33:13 +0300

    feat(repositories): exists method, too long content error handing

--- auth_service/src/repositories/auth.py
+++ auth_service/src/repositories/auth.py
@@ -1,10 +1,11 @@
 from typing import TYPE_CHECKING
+from uuid import UUID
 
-from sqlalchemy.exc import NoResultFound, IntegrityError
-from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound, IntegrityError, DBAPIError
+from sqlalchemy import select, exists, or_
 
 from src.models.auth import AuthCredentials
-from src.exceptions.repositories import RowDoesNotExist, RowAlreadyExists
+from src.exceptions.repositories import RowDoesNotExist, RowAlreadyExists, ColumnContentTooLongError
 
 if TYPE_CHECKING:
     from src.core.database import AsyncDatabase
@@ -27,8 +28,26 @@ class AuthRepository:
             except NoResultFound as e:
                 raise RowDoesNotExist(f"Unable to find row with {login=}") from e
             return credentials
+        
+    async def exists(self, id: UUID | None = None, login: str | None = None) -> bool:
+        async with self.db.get_session() as session:
+            if not any([id, login]):
+                return False
+            
+            query = select(exists().where(
+                or_(
+                    *(clause for clause in 
+                        [self.model.id == id if id else None,
+                        self.model.login == login if login else None]  
+                        if clause is not None
+                    )
+                )
+            ))
+            result = await session.execute(query)
 
-    async def create_one(self, credentials: AuthCredentials) -> int:
+            return bool(result.scalar())
+
+    async def create_one(self, credentials: AuthCredentials) -> UUID:
         async with self.db.get_session() as session:
             session.add(credentials)
             try:
@@ -38,6 +57,10 @@ class AuthRepository:
             except IntegrityError as e:
                 raise RowAlreadyExists(
                     f"Row with login - {credentials.login} already exists"
+                ) from e
+            except DBAPIError as e:
+                raise ColumnContentTooLongError(
+                    f"Column login - {credentials.login} too long"
                 ) from e
 
             return new_credentials_id
