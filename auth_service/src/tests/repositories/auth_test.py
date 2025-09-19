7a9e8f24a29f8ba7cea1730458aada77a7a8dd3d
Ganechka-web <maksim15848@gmail.com>
2025-09-19 12:53:23 +0300

    feat(tests): repositories conftest refactoring and tests

--- auth_service/src/tests/repositories/auth_test.py
+++ auth_service/src/tests/repositories/auth_test.py
@@ -1,17 +1,88 @@
 from typing import TYPE_CHECKING
+from uuid import UUID
+from contextlib import nullcontext as does_not_raise
+from unittest import mock
 
 import pytest
+
+from src.models.auth import AuthCredentials
+from src.exceptions.repositories import (
+    RowDoesNotExist,
+    RowAlreadyExists,
+    ColumnContentTooLongError,
+)
 
 if TYPE_CHECKING:
     from src.repositories.auth import AuthRepository
 
 
-@pytest.mark.asyncio
 class TestAuthRepository:
-    @pytest.mark.usefixtures("insert_test_data")
     @pytest.mark.parametrize(
-        ("login",), (("test_user_1",), ("test_user_2",), ("test_user_3",))
+        ("login", "expected_login", "exception"),
+        (
+            ("test_user_1", "test_user_1", does_not_raise()),
+            ("test_user_2", "test_user_2", does_not_raise()),
+            ("test_unexistent_user", None, pytest.raises(RowDoesNotExist)),
+        ),
     )
-    async def test_get_one_by_login(self, login: str, get_auth_repository: "AuthRepository"):
-        auth_creadentials = await get_auth_repository.get_one_by_login(login=login)
-        assert auth_creadentials.login == login
+    @pytest.mark.asyncio
+    async def test_get_one_by_login(
+        self, login, expected_login, exception, auth_repository: "AuthRepository"
+    ):
+        with exception:
+            credentials = await auth_repository.get_one_by_login(login=login)
+
+            assert credentials.login == expected_login
+
+    @pytest.mark.parametrize(
+        ("login", "password", "exception"),
+        (
+            ("creation_test_1", "some@password@hash", does_not_raise()),
+            ("creation_log_test_1", "some@password@hash", does_not_raise()),
+            (
+                "creation_too_long_login_test_2",
+                "1233@klxmk@",
+                pytest.raises(ColumnContentTooLongError),
+            ),
+            ("creation_test_1", "jj22k2kk", pytest.raises(RowAlreadyExists)),
+        ),
+    )
+    @pytest.mark.asyncio
+    async def test_create_one(
+        self, login, password, exception, auth_repository: "AuthRepository"
+    ):
+        with exception:
+            auth_credentials = AuthCredentials(login=login, password=password)
+            auth_credentials_id = await auth_repository.create_one(
+                credentials=auth_credentials
+            )
+            created_auth_credentials = await auth_repository.get_one_by_login(
+                login=login
+            )
+
+            assert isinstance(auth_credentials_id, UUID) is True
+            assert login == created_auth_credentials.login
+            assert password == created_auth_credentials.password
+
+    @pytest.mark.parametrize(
+        ("login", "exception"),
+        (("test_user_1", does_not_raise()), ("test_user_2", does_not_raise())),
+    )
+    @pytest.mark.asyncio
+    async def test_delete_one(
+        self, login, exception, auth_repository: "AuthRepository"
+    ):
+        with exception:
+            auth_credentials_on_delete = await auth_repository.get_one_by_login(
+                login=login
+            )
+
+            auth_repository.db.session.delete = mock.AsyncMock()
+            auth_repository.db.session.commit = mock.AsyncMock()
+
+            await auth_repository.delete_one(credentials=auth_credentials_on_delete)
+
+            auth_repository.db.session.delete.assert_awaited_once_with(
+                auth_credentials_on_delete
+            )
+            auth_repository.db.session.commit.assert_awaited_once()
--- auth_service/src/tests/repositories/conftest.py
+++ auth_service/src/tests/repositories/conftest.py
@@ -1,29 +1,11 @@
-import json
 from typing import TYPE_CHECKING
 
-import pytest_asyncio
-from sqlalchemy import text
-
-from src.models.auth import AuthCredentials
-from src.repositories.auth import AuthRepository
+import pytest
 
 if TYPE_CHECKING:
-    from tests.db.database import TestDatabase
+    from src.repositories.auth import AuthRepository
 
 
-@pytest_asyncio.fixture
-async def insert_test_data(get_test_db: "TestDatabase"):
-    async with get_test_db.async_database.get_session() as session:
-        # gets test data from file
-        with open(r"src/tests/repositories/test_db_data.json", "r", encoding="utf-8") as file:
-            entities = [AuthCredentials(**entity) for entity in json.load(file)]
-        session.add_all(entities)
-        await session.commit()
-        yield
-        # delete all rows
-        await session.execute(text("TRUNCATE TABLE auth_credentials"))
-
-
-@pytest_asyncio.fixture
-async def get_auth_repository(get_test_db: "TestDatabase"):
-    return AuthRepository(database=get_test_db.async_database)
+@pytest.fixture(scope="session")
+def auth_repository(container, insert_test_data) -> "AuthRepository":
+    return container.auth_repository()
