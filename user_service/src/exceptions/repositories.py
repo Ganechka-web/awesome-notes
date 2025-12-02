883b74759b832a6243f97b0210bcbb4974214c63
Ganechka-web <maksim15848@gmail.com>
2025-12-02 09:54:50 +0300

    feat(exceptions): new repository exceptions

--- user_service/src/exceptions/repositories.py
+++ user_service/src/exceptions/repositories.py
@@ -2,3 +2,11 @@ class DataBaseError(Exception):
     def __init__(self, msg: str, *args):
         self.msg = msg
         super().__init__(*args)
+
+
+class NoSuchRowError(DataBaseError):
+    pass
+
+
+class RowAlreadyExists(DataBaseError):
+    pass
