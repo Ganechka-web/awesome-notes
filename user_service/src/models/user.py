59a2efb139817089fa40b1cba6d32b974f4805eb
Ganechka-web <maksim15848@gmail.com>
2025-12-05 15:28:57 +0300

    feat(models, alembic): change age type to SmallInteger, migrations

--- user_service/src/models/user.py
+++ user_service/src/models/user.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from uuid import UUID, uuid4
 
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String, text
+from sqlalchemy import String, text, SmallInteger
 from sqlalchemy import UUID as SQL_UUID
 
 from src.core.database import Base
@@ -24,7 +24,7 @@ class User(Base):
     id: Mapped[UUID] = mapped_column(SQL_UUID, primary_key=True, default=uuid4)
     username: Mapped[str] = mapped_column(String(50), unique=True)
     gender: Mapped[Gender] = mapped_column(default=Gender.unknown)
-    age: Mapped[int]
+    age: Mapped[int] = mapped_column(SmallInteger)
 
     joined_at: Mapped[datetime] = mapped_column(server_default=SQL_TIMEZONE_NOW)
     updated_at: Mapped[datetime] = mapped_column(
